3b1915212436ac1d940c3a742ec744250a9695e9
wesvh <65504680+wesvh@users.noreply.github.com>
2022-06-04 15:52:54 -0500

    Modificado, solo ejecucion de metodos

--- MainTemporal.java
+++ MainTemporal.java
@@ -1,65 +1,15 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+package Ferreteria;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class MainTemporal {
     
     public static void main(String[] args) throws SQLException {
-        //Se agregan las credenciales y link de acceso a la base de datos
-        String dbURL= "jdbc:mysql://localhost:3306/ferreteria"; //luego de localhost:3306/  se agrega el nombre del schema 
-        String username = "root"; //credenciales
-        String password = "root";
-        Connection conn= null; //se inicializa la variable que comprueba conexion
-        try{
-        conn= DriverManager.getConnection(dbURL,username,password); //conecta
-        if (conn!=null) {
-            System.out.println("Conectado");  //Si no es null nos devuelve el mensaje
-        }
-    } catch(SQLException ex){
-        ex.printStackTrace();}
-        
-        //Se inicializa la variable que contendra el script de INSERT   
-        String insert = "INSERT INTO  productos (nombre,referencia,valor_compra,valor_venta,cantidad,categoria) VALUES (?,?,?,?,?,?)";
-        PreparedStatement INSERT = conn.prepareStatement(insert);
-        //Se reemplaza cada ? , en orden siendo (x,valor) x el numero del ? a reemplazar por "valor"
-        INSERT.setString(1 , "PRUEBA DE CONEXION"); 
-        INSERT.setString(2 , "R0003");
-        INSERT.setInt(3 , 1000);
-        INSERT.setInt(4 , 2500);
-        INSERT.setInt(5 , 50);
-        INSERT.setString(6 , "PVC");
-        int rowsInserted = INSERT.executeUpdate(); //ejecuta el script de SQL
-        if ( rowsInserted > 0) {
-        System.out.println (" Insercion exitosa !"); //Devuelve esto si se logro hacer la insercion
-        }
-        // CONSULTAS
-        String select = "SELECT * FROM productos" ; //Script de la consulta a listar
-        Statement SELECT = conn.createStatement();
-        ResultSet result =SELECT.executeQuery(select); //Se le otorga la variable de select
-        while (result.next()){
-        String nombre = result.getString(1); //Se establece en el GET el numero correspondiente al valor dentro de la lista a desear obtener
-        int cantidad = result.getInt(5);
-            System.out.println("Nombre del producto: " + nombre + " Cantidad de ese producto: " + cantidad);} 
-        
-        //SCRIPT DE UPDATE
-        
-        String update = "UPDATE productos SET valor_compra =? WHERE referencia=?"; //Un ejemplo de actualizacion de precio en funcion del numero de referencia
-        PreparedStatement UPDATE = conn.prepareStatement(update);
-        UPDATE.setInt(1, 50000); //SE REEMPLAZA IGUAL QUE EN LO ANTERIOR, PRIMERO EL NUMERO DEL '?' y luego el valor a poner sobre èl.
-        UPDATE.setString(2,"R0003");
-        int rowsUpdated = UPDATE.executeUpdate(); //Se enumera la cantidad de actualizaciones
-        if (rowsUpdated > 0) {
-            System.out.println("EL REGISTRO SE ACTUALIZO CORRECTAMENTE"); //Se imprime si hay mas de 1 actualizacion.
-        //SCRIPT DE BORRADO
-        String delete = "DELETE FROM productos WHERE referencia=?";  //Un ejemplo de borrar un dato en funcion de la referencia
-        PreparedStatement DELETE = conn.prepareStatement(delete);
-        DELETE.setString(1,"R0003"); // Mismo caso de reemplazo
-        int rowsDeleted = DELETE.executeUpdate(); //FUNCION DE ELIMINAR
-        if (rowsDeleted>0) {
-            System.out.println("BORRADO EXITOSO");            
-        }
-        
-        }}}
+        Conexion exec = new Conexion();
+        exec.EstableciendoConexion();
+        exec.agregarProductos();
+        exec.modificarProductos();
+        exec.consultarProductos();
+        exec.borrarProductos();
+
+     
+        }}
