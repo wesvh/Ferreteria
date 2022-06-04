f0fe13e7653c83d2ee057530adb8c48bb34954ae
wesvh <65504680+wesvh@users.noreply.github.com>
2022-06-04 13:01:13 -0500

    Update MainTemporal.java
    Comentarios

--- MainTemporal.java
+++ MainTemporal.java
@@ -8,30 +8,30 @@ import java.sql.Statement;
 public class MainTemporal {
     
     public static void main(String[] args) throws SQLException {
-        String dbURL= "jdbc:mysql://localhost:3306/ferreteria";
-        String username = "root";
+        //Se agregan las credenciales y link de acceso a la base de datos
+        String dbURL= "jdbc:mysql://localhost:3306/ferreteria"; //luego de localhost:3306/  se agrega el nombre del schema 
+        String username = "root"; //credenciales
         String password = "root";
-        Connection conn= null;
+        Connection conn= null; //se inicializa la variable que comprueba conexion
         try{
-        conn= DriverManager.getConnection(dbURL,username,password);
+        conn= DriverManager.getConnection(dbURL,username,password); //conecta
         if (conn!=null) {
-            System.out.println("Conectado");            
+            System.out.println("Conectado");  //Si no es null nos devuelve el mensaje
         }
     } catch(SQLException ex){
         ex.printStackTrace();}
-      
-        
+        //Se inicializa la variable que contendra el script de INSERT   
         String sql = "INSERT INTO  productos (nombre,referencia,valor_compra,valor_venta,cantidad,categoria) VALUES (?,?,?,?,?,?)";
         PreparedStatement statement = conn.prepareStatement(sql);
-
-statement.setString(1 , "PRUEBA DE CONEXION");
-statement.setString(2 , "R0003");
-statement.setInt(3 , 1000);
-statement.setInt(4 , 2500);
-statement.setInt(5 , 50);
-statement.setString(6 , "PVC");
-int rowsInserted = statement.executeUpdate();
-if ( rowsInserted > 0) {
-System.out.println (" Insercion exitosa !");
+        //Se reemplaza cada ? , en orden siendo (x,valor) x el numero del ? a reemplazar por "valor"
+        statement.setString(1 , "PRUEBA DE CONEXION"); 
+        statement.setString(2 , "R0003");
+        statement.setInt(3 , 1000);
+        statement.setInt(4 , 2500);
+        statement.setInt(5 , 50);
+        statement.setString(6 , "PVC");
+        int rowsInserted = statement.executeUpdate(); //ejecuta el script de SQL
+        if ( rowsInserted > 0) {
+        System.out.println (" Insercion exitosa !"); //Devuelve esto si se logro hacer la insercion
                
     }}}
